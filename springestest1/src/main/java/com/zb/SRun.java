6993512c3a549ff348379b4d033663f50753570c
wangxu <1578458207@qq.com>
2020-03-01 16:02:20 +0800

    修改了代码

---
+++ springestest1/src/main/java/com/zb/SRun.java
@@ -0,0 +1,14 @@
+package com.zb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SRun {
+    public static void main(String[] args) {
+        //测试提交 本地仓库 至远程仓库
+        //哥哥修改了代码
+        //修改了代码
+        SpringApplication.run(SRun.class,args);
+    }
+}
