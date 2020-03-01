c113708fbcb393f9dff681026495a4c6528814e1
wangxu <1578458207@qq.com>
2020-03-01 14:57:11 +0800

    update

--- springestest/src/main/java/com/zb/SRun.java
+++ springestest/src/main/java/com/zb/SRun.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SRun {
     public static void main(String[] args) {
+        //测试提交 本地仓库 至远程仓库
         SpringApplication.run(SRun.class,args);
     }
 }
